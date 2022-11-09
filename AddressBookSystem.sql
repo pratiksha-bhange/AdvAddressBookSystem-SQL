4f5c6301fb9d078f374ac90634df68e44a0787e8
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-11-09 13:58:30 +0530

    [Pratu]ADDED: Create Address Book Table

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -1,3 +1,7 @@
 --UC1 - Create AddressBook_Service Database.
 create database AddressBook_Service;
 
+--UC2 - Create AddressBook Table
+create table AddressBook_Table(FirstName varchar(100),LastName varchar(100),Address varchar(100),City varchar(100),State varchar (100),Zip bigint,PhoneNumber bigint,Email varchar(100));
+select * from AddressBook_Table;
+
